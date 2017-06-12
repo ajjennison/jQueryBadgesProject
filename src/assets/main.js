f742b81163869c532f649566dca15ef291ae7e47
ajjennison <alexander.jennison@gmail.com>
2017-06-11 22:10:41 -0700

    Implemented AJAX call
    Ajax call pulls in badge info and the success callback builds a div for
    each badge in the HTML document

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,21 @@
 $(function() {
 
-  // your code will go here
+  $.ajax ({
+    url: "https://www.codeschool.com/users/3364148.json",
+    dataType: "jsonp",
+    success: function(response) {
+      console.log(response);
+      let completedCourses = response.courses.completed;
+      for(let i=0; i < completedCourses.length; i++) {
+        let newBadge = '<div class="course">';
+        newBadge += "<h3>" + completedCourses[i].title + "</h3>";
+        newBadge += '<img src="' + completedCourses[i].badge + '"></img>';
+        newBadge += '<a href="' + completedCourses[i].url +
+        '" target="_blank" class="btn btn-primary">' + 'See Course' + '</a>';
+        newBadge += '</div>';
+        $("#badges").append(newBadge);
+      }
+    }
+  });
 
 });
